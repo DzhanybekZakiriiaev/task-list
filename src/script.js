6daa0a52b96cd5d1807fed843b8a5f6197795f0a
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-04-30 21:57:14 +0600

    added the ability to cross out a task when double-clicking, removed task highlighting

--- src/script.js
+++ src/script.js
@@ -1,14 +1,26 @@
+'use strict';
+
 const form = document.querySelector('#addTaskForm');
 const taskInput = document.querySelector('#taskInput');
 const taskList = document.querySelector('#taskList');
 form.addEventListener('submit', createTask);
-function createTask(e){
+
+function createTask(e) {
     e.preventDefault();
     const taskText = taskInput.value;
     const taskListItem = document.createElement('li');
     taskListItem.textContent = taskText;
     taskListItem.classList.add('list-group-item');
+    taskListItem.style.userSelect = 'none';
     taskList.appendChild(taskListItem);
     taskInput.value = '';
     taskInput.focus();
 }
+
+const taskElem = document.querySelectorAll(".list-group");
+
+taskElem.forEach((taskElement) => {
+    taskElement.addEventListener("dblclick", (event) => {
+        event.target.style.textDecoration = "line-through";
+    });
+});
